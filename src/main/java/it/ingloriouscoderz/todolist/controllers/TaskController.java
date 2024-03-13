680a4a38b76f95248fc943bd5884e36d7980d1c1
Matteo Antony Mistretta <m.mistretta@reply.it>
2024-03-13 16:31:56 +0100

    Implement all CRUD operations

--- src/main/java/it/ingloriouscoderz/todolist/controllers/TaskController.java
+++ src/main/java/it/ingloriouscoderz/todolist/controllers/TaskController.java
@@ -3,16 +3,20 @@ package it.ingloriouscoderz.todolist.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.ingloriouscoderz.todolist.model.Task;
 
 @RestController
-@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RequestMapping("/tasks")
 public class TaskController {
   private List<Task> tasks;
 
@@ -23,16 +27,42 @@ public class TaskController {
     tasks.add(new Task(3, "Forget everything", false));
   }
 
-  @GetMapping("/tasks")
+  @GetMapping
   public List<Task> getTasks() {
     return tasks;
   }
 
-  @PostMapping(value = "/tasks", consumes = "application/json", produces = "application/json")
+  @PostMapping
   public Task create(@RequestBody Task body) {
-    int maxId = tasks.size() > 0 ? tasks.get(tasks.size() - 1).id(): 0;
+    int maxId = tasks.size() > 0 ? tasks.get(tasks.size() - 1).id() : 0;
     Task task = new Task(maxId + 1, body.text(), body.completed());
     tasks.add(task);
+    return task;
+  }
+
+  @PutMapping("/{id}")
+  public Task replace(@PathVariable int id, @RequestBody Task body) {
+    Task taskToReplace = tasks.stream().filter(t -> t.id() == id).findAny().orElse(null);
+    int index = tasks.indexOf(taskToReplace);
+    Task task = new Task(id, body.text(), body.completed());
+    tasks.set(index, task);
+    return task;
+  }
+
+  @PatchMapping("/{id}")
+  public Task update(@PathVariable int id, @RequestBody Task body) {
+    Task taskToReplace = tasks.stream().filter(t -> t.id() == id).findAny().orElse(null);
+    int index = tasks.indexOf(taskToReplace);
+    Task task = new Task(id, body.text() != null ? body.text() : taskToReplace.text(),
+        body.completed() != null ? body.completed() : taskToReplace.completed());
+    tasks.set(index, task);
+    return task;
+  }
+
+  @DeleteMapping("/{id}")
+  public Task delete(@PathVariable int id) {
+    Task task = tasks.stream().filter(t -> t.id() == id).findAny().orElse(null);
+    tasks.remove(task);
     return task;
   }
 }
--- src/main/java/it/ingloriouscoderz/todolist/model/Task.java
+++ src/main/java/it/ingloriouscoderz/todolist/model/Task.java
@@ -1,39 +1,4 @@
 package it.ingloriouscoderz.todolist.model;
 
-public record Task(int id, String text, boolean completed) {}
-
-// public class Task {
-//   private int id;
-//   private String text;
-//   private boolean isCompleted;
-
-//   public Task(int id, String text, boolean isCompleted) {
-//     this.id = id;
-//     this.text = text;
-//     this.isCompleted = isCompleted;
-//   }
-
-//   public int getId() {
-//     return id;
-//   }
-  
-//   public void setId(int id) {
-//     this.id = id;
-//   }
-  
-//   public String getText() {
-//     return text;
-//   }
-  
-//   public void setText(String text) {
-//     this.text = text;
-//   }
-  
-//   public boolean isCompleted() {
-//     return isCompleted;
-//   }
-  
-//   public void setCompleted(boolean isCompleted) {
-//     this.isCompleted = isCompleted;
-//   }
-// }
+public record Task(int id, String text, Boolean completed) {
+}
